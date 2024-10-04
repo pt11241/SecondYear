2cbe0da9509a8a687fc35e91d1eabd083d751eb2
pt11241 <html23232@gmail.com>
2024-10-04 16:15:58 +0300

    F

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -4,29 +4,55 @@ class ArrayHandler {
 private:
     size_t _size;
     T* _array;
+    size_t _cnt;
+    // T max = -1000000000;
+    // T min = 1000000000; 
 public:
     ArrayHandler(size_t size = 10) {
-
+        _size = size;
+        _cnt = 0;
+        _array = new T[_size];
+       
+    }
+    void AppendElem(T elem){
+        if (_cnt == _size){
+            T* array = new T[_size * _size];
+            for (size_t i = 0; i < _size; i++){
+                array[i] = _array[i];
+            }
+            delete [] _array;
+            _array = array;
+            _size = _size * _size; 
+        }
+        _array[_cnt++] = elem;
+        
     }
 
-    void AppendElem(T elem) {
-
-    }
-
-    bool IsContains(T elem) {
+    bool IsContains(const T& elem)const{
+        for (size_t i = 0; i < _cnt; i++){
+            if (_array[i] == elem) return true;
+        }
         return false;
     }
 
-    T GetMax() {
-        return 0;
+    T GetMax(){
+        T max = _array[0];
+        for(size_t i = 1; i < _cnt; i++){
+            if (_array[i] > max) max = _array[i];
+        } 
+        return max;
     }
 
     T GetMin() {
-        return 0;
+        T min = _array[0];
+        for (size_t i = 1; i < _cnt; i++){
+            if (_array[i] < min) min = _array[i];
+        }
+        return min;
     }
 
     ~ArrayHandler() {
-
+        delete [] _array;
     }
 
 };
