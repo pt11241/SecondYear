3a36ffba093e7faae72de1275d53e93166f8c2c2
brb484 <html23232@gmail.com>
2024-10-08 17:00:41 +0300

    upd

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -6,25 +6,34 @@ private:
     size_t _size;
     T* _array;
     size_t _cnt;
-    // T max = -1000000000;
-    // T min = 1000000000; 
+    T _max;
+    T _min; 
 public:
-    ArrayHandler(size_t size = 10) {
+    ArrayHandler(size_t size = 100000000) {
         _size = size;
         _cnt = 0;
         _array = new T[_size];
        
     }
     void AppendElem(T elem){
-        if (_cnt == _size){
-            T* array = new T[_size * _size];
-            for (size_t i = 0; i < _size; i++){
-                array[i] = _array[i];
-            }
-            delete [] _array;
-            _array = array;
-            _size = _size * _size; 
+        // if (_cnt == _size){
+        //     T* array = new T[_size * 10000000];
+        //     for (size_t i = 0; i < _size; i++){
+        //         array[i] = _array[i];
+        //     }
+        //     delete [] _array;
+        //     _array = array;
+        //     _size = _size * 10000000; 
+        // }
+        if (_cnt != 0){
+            _min = (elem < _min) ? elem : _min;
+            _max = (elem > _max) ? elem : _max;
         }
+        else{
+            _min = elem;
+            _max = elem;
+        }
+
         _array[_cnt++] = elem;
         
     }
@@ -36,21 +45,32 @@ public:
         return false;
     }
 
-    T GetMax(){
-        T max = _array[0];
-        for(size_t i = 1; i < _cnt; i++){
-            if (_array[i] > max) max = _array[i];
-        } 
-        return max;
+    // T GetMax(){
+    //     T max = _array[0];
+    //     for(size_t i = 1; i < _cnt; i++)
+    //     {
+    //         if (_array[i] > max) max = _array[i];
+    //     }
+    //     return max;
+    // };
+
+    // T GetMin(){
+    //     T min = _array[0];
+    //     for(size_t i = 1; i < _cnt; i++)
+    //     {
+    //         if (_array[i] < min) min = _array[i];
+    //     }
+    //     return min;
+    // };
+
+    T GetMin(){
+        return _min;
     }
 
-    T GetMin() {
-        T min = _array[0];
-        for (size_t i = 1; i < _cnt; i++){
-            if (_array[i] < min) min = _array[i];
-        }
-        return min;
+    T GetMax(){
+        return _max;
     }
+
 
     ~ArrayHandler() {
         delete [] _array;
